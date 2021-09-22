89e8e0582dbd1927a7d28cad08437d4e1836421b
Jinendra Sontakke <63655965+JinendraSontakke@users.noreply.github.com>
2021-09-23 00:49:08 +0530

    Add files via upload

---
+++ dec_var.java
@@ -0,0 +1,16 @@
+public class dec_var {
+    public static void main(String[] args){
+        // declaring a variable 
+        int speed = 80;
+
+        // another way to declare a variable
+        int highspeed;
+        // assigning value to variable 
+        highspeed = 100;
+
+        // printing the value of speed and highspeed
+        System.out.println(speed);
+        System.out.println(highspeed);
+    }
+    
+}
