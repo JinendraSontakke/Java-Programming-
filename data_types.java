f2921d4b009e5094f973620d7eba2272d0915751
Jinendra Sontakke <63655965+JinendraSontakke@users.noreply.github.com>
2021-09-23 01:19:17 +0530

    Add files via upload

---
+++ data_types.java
@@ -0,0 +1,25 @@
+public class data_types {
+    public static void main(String[] args) {
+
+        boolean flag = true;
+        byte range = 124;
+        short temp = -200;
+        int num = 45000;
+        long curr = 65234000;
+        float dec = (float) 23.5;
+        double decc = -2.45666666;
+        char ch_val = 'g';
+        String str = "java programming";
+
+        System.out.println(flag);
+        System.out.println(range);
+        System.out.println(temp);
+        System.out.println(curr);
+        System.out.println(dec);
+        System.out.println(decc);
+        System.out.println(ch_val);
+        System.out.println(str);
+        
+    }
+    
+}
